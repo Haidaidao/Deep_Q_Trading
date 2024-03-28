2eac33e1116c99b370741755343d8a2356acad90
khoi.le <khoi.le@v-key.com>
2024-03-28 10:31:07 +0700

    Add stop-loss adjusted label

--- ensemble.py
+++ ensemble.py
@@ -35,33 +35,84 @@ def getActionWeek(weeksFrame, date):
             return  weeksFrame['ensemble'][i-1]
     return 0
 
-# def ensemble_y_true(df1, df2, df3):
+iter = 0
 
-#     df = pd.DataFrame(columns=['ensemble'])
-#     df = df.set_index(pd.Index([], name='date'))
+def stop_loss_labeling(feature, stats, control_window, delta):
+    global iter
+    file = open(f'label-{iter}.txt', 'w')
+ 
+    def get_next_index(index):
+        try:
+            next_i = stats.index[stats.index.get_loc(index) + control_window]
+        except:
+            return None
+        finally:
+            return next_i
+    
+    labels = []
+    last_action = 0
+    count = 0
+    command_close = 0
+    # long short labeling
+    for index, _ in feature.iterrows():
+        if index not in stats.index:
+            labels.append(last_action)
+            continue
 
-#     for k in range(0,len(df1)):
-#         if(df1.index[k] in df2.index):
-#             if df1['ensemble'][k] == 0:
-#                 df.loc[df1.index[k]] = 0
-#             else:
-#                 if df1['ensemble'][k] == getActionWeek(df3, df2.index[k]) or df1['ensemble'][k] == df2.loc[df1.index[k],'ensemble']: 
-#                     if  getActionWeek(df3, df2.index[k]) == df2.loc[df1.index[k],'ensemble']  and df2.loc[df1.index[k],'ensemble'] != df1['ensemble'][k] :
-#                         df.loc[df1.index[k]] = 0
-#                     elif df2.loc[df1.index[k],'ensemble'] == 0 and getActionWeek(df3, df2.index[k]) !=0:
-#                         df.loc[df1.index[k]] = getActionWeek(df3, df2.index[k])
-#                     elif df2.loc[df1.index[k],'ensemble'] != 0 and getActionWeek(df3, df2.index[k]) ==0:
-#                         df.loc[df1.index[k]] = df2.loc[df1.index[k],'ensemble']
-#                     elif getActionWeek(df3, df2.index[k]) != df2.loc[df1.index[k],'ensemble']:
-#                         df.loc[df1.index[k]] = 0
-#                     elif getActionWeek(df3, df2.index[k]) == df2.loc[df1.index[k],'ensemble']:
-#                         df.loc[df1.index[k]] = df2.loc[df1.index[k],'ensemble']
-#                     else:
-#                         df.loc[df1.index[k]] = 0
-#                 else: 
-#                     df.loc[df1.index[k]] = 0
+        n_index = get_next_index(index)
+        if n_index == None:
+            labels.append(0)
+            continue
 
-#     return df['ensemble'].tolist()
+        if last_action == 0:
+            closes = (stats.loc[index, 'Close'], stats.loc[n_index, 'Close']) # price of index, price of index + control_window (upper delta)
+            low = stats.loc[index:n_index, 'Low'].min() # lowest price of in the control_window starting from index
+
+            first_half = closes[1] / closes[0] # p(t + large delta) / p(t)
+            second_half = low / closes[0] # min(from t to delta) / p(t)
+
+            action = 0
+            if first_half > 1 and second_half >= 1 - delta: action = 1 # long action 
+            elif first_half < 1 and second_half <= 1 - delta: action = 2 # short action
+
+            if action != 0: # if a trade command start, stored the close price when start trading
+                count = 0
+                command_close = closes[0]
+                last_action = action
+
+            labels.append(action)
+        else: # holding asset 
+            close = stats.loc[index, 'Close'] # current s close price in (t, t + upper delta)
+            if last_action == 1 and close / command_close < 1 - delta: # end long trade (sell asset)
+                labels.append(-1)
+                last_action
+            elif last_action == 2 and close / command_close > 1 - delta:  # end short trade (reversal of long action) 
+                labels.append(-1) 
+                last_action = 0
+            elif count == control_window - 1: # ending trade after a time set of control_window time (sell asset at t + upper delta), 
+                labels.append(-1)
+                last_action = 0
+            else: # continue holding the asset
+                count+=1 
+                labels.append(0)
+
+    file.write(f"{labels}\n")
+
+    # file.close(); iter += 1
+    # return labels
+   
+    # normalize for Evaluation from (-1 - 0 - 1 - 2) or (sell - hold - long trade - short trade) to (-1 - 0 - 1) or (sell - hold - buy) 
+    finale_labels = []
+    for label in labels:
+        if (label == 2): 
+            finale_labels.append(1)
+        else:
+            finale_labels.append(label)
+
+    file.write(f"{finale_labels}")
+    
+    file.close(); iter += 1
+    return finale_labels
 
 def ensemble_y_true(feature, stats, threshold):
 
@@ -74,7 +125,7 @@ def ensemble_y_true(feature, stats, threshold):
             labels.append(last_action)
             skip += 1
             continue
-        
+
         close = stats.loc[index, 'Close']
         open = stats.loc[index, 'Open']
         
@@ -243,8 +294,14 @@ def XGBoostEnsemble(numWalks,type,numDel):
     values.append([' ','Sum',str(round(current_balance_sum,2)),str(round(net_profit_sum,2)),str(round(winning_trade_number_sum,2)),str(round(losing_trade_number_sum,2)),str(round(profit_factor_sum,2)),str(round(percent_profitable_sum,2)) + '%',str(round(average_profit_per_trade_sum,2))])
     # print(values)
     return values,columns
+
+re_iter = 0
 # ================================================ Random Forest
 def RandomForestEnsemble(numWalks,type,numDel):
+
+    global re_iter
+
+    file = open(f'label-comparison-{re_iter}.txt', 'w')
     
     current_balance_sum=0
     net_profit_sum=0
@@ -298,7 +355,11 @@ def RandomForestEnsemble(numWalks,type,numDel):
         for k in range(0,len(df1)):
             list_combine_train = np.append(list_combine_train, [[df1['ensemble'][k], df2['ensemble'][k], df3_temp['ensemble'][k]]], axis=0)
 
-        y_train = ensemble_y_true(df1, dax, threshold)
+        y_train_og = ensemble_y_true(df1, dax, threshold)
+        y_train = stop_loss_labeling(df1, dax, 3, 0.005)
+
+        file.write(f'Stop-loss adjusted: {y_train}\n')
+        file.write(f'    Old           : {y_train_og}\n=======\n')
 
         # rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
         rf_model.fit(list_combine_train, y_train)
@@ -366,6 +427,8 @@ def RandomForestEnsemble(numWalks,type,numDel):
 
     values.append([' ','Sum',str(round(current_balance_sum,2)),str(round(net_profit_sum,2)),str(round(winning_trade_number_sum,2)),str(round(losing_trade_number_sum,2)),str(round(profit_factor_sum,2)),str(round(percent_profitable_sum,2)) + '%',str(round(average_profit_per_trade_sum,2))])
     # print(values)
+
+    file.close(); re_iter +=1
     return values,columns
 # ================================================ Base-rule
 
