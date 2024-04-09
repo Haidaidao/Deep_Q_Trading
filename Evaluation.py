3fdb48d1d97d5bd256e20f4fb72be25379a18244
Haidaidao <hai0944345963@gmail.com>
2024-04-09 12:07:42 +0700

    fix ensemble

--- Evaluation.py
+++ Evaluation.py
@@ -11,6 +11,36 @@ class Evaluation:
     def __init__(self, data):
         self.data = data
 
+    # def evaluate(self):
+    #     wins = 0
+    #     loses = 0
+    #     profit = 0
+
+    #     for i in range(len(self.data)-1):
+    #         signal = self.data.iloc[i]['ensemble']
+    #         entry_price = self.data.iloc[i]['close'] 
+    #         entry_price_open = self.data.iloc[i]['open'] 
+    #         next_price_low = self.data.iloc[i+1]['low'] 
+    #         next_price_high = self.data.iloc[i+1]['high'] 
+
+    #         if signal == 1:
+    #             if next_price_high >= entry_price * 1.01:  # Takeprofit
+    #                 wins += 1
+    #                 profit += entry_price - entry_price_open
+    #             elif next_price_low <= entry_price * 0.995:  # Stoploss
+    #                 loses += 1
+    #                 profit -= entry_price - entry_price_open
+            
+    #         elif signal == 2 or signal == -1:
+    #             if next_price_low <= entry_price * 0.99:  # Takeprofit
+    #                 wins += 1
+    #                 profit += entry_price - entry_price_open
+    #             elif next_price_high >= entry_price * 1.005:  # Stoploss
+    #                 loses += 1
+    #                 profit -= entry_price - entry_price_open
+    #     return wins, loses, profit
+        
+
     def evaluate(self):
         wins = 0
         loses = 0
@@ -26,18 +56,16 @@ class Evaluation:
             if signal == 1:
                 if next_price_high >= entry_price * 1.01:  # Takeprofit
                     wins += 1
-                    profit += entry_price - entry_price_open
                 elif next_price_low <= entry_price * 0.995:  # Stoploss
                     loses += 1
-                    profit -= entry_price - entry_price_open
+                profit += entry_price - entry_price_open
             
             elif signal == 2 or signal == -1:
                 if next_price_low <= entry_price * 0.99:  # Takeprofit
                     wins += 1
-                    profit += entry_price - entry_price_open
                 elif next_price_high >= entry_price * 1.005:  # Stoploss
                     loses += 1
-                    profit -= entry_price - entry_price_open
+                profit -= entry_price - entry_price_open
         return wins, loses, profit
 
 
