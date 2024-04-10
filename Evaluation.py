ebc5e96f64196d610e7b0c7e3613ad0a7296d5ca
Haidaidao <hai0944345963@gmail.com>
2024-04-10 13:58:53 +0700

    fix ensemble

--- Evaluation.py
+++ Evaluation.py
@@ -56,16 +56,18 @@ class Evaluation:
             if signal == 1:
                 if next_price_high >= entry_price * 1.01:  # Takeprofit
                     wins += 1
+                    profit += entry_price*0.1
                 elif next_price_low <= entry_price * 0.995:  # Stoploss
                     loses += 1
-                profit += entry_price - entry_price_open
+                    profit -= entry_price*0.05
             
             elif signal == 2 or signal == -1:
                 if next_price_low <= entry_price * 0.99:  # Takeprofit
                     wins += 1
+                    profit += entry_price*0.1
                 elif next_price_high >= entry_price * 1.005:  # Stoploss
                     loses += 1
-                profit -= entry_price - entry_price_open
+                    profit -= entry_price*0.05
         return wins, loses, profit
 
 
